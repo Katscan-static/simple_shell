bb6bd0a24ce4411b2ff813d418e2ea10d3d72b28
Katekani Madingana <madinganakn@gmail.com>
2023-04-26 08:13:21 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+extern char **environ;
+
+void execute(char **args);
+void _puts(char *str);
+int _putchar(char c);
+void handle_interrupt(int signal);
+void tokenize_line(char **args, char **line);
+void is_interactive(void);
+#endif
